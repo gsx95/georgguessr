e287e31ce299a0e6e5f6db96347dd84700141ad1
gsx95 <g.schwarz95@me.com>
2021-01-02 16:04:27 +0100

    home page skeleton

---
+++ statics/js/home.js
@@ -0,0 +1,3 @@
+function showHome() {
+    renderWholeView("HomeTemplate");
+}
---
+++ statics/js/render.js
@@ -0,0 +1,15 @@
+let mainDiv;
+
+function initRenderer() {
+    mainDiv = document.getElementById("main");
+}
+
+function renderWholeView(templName, params) {
+    var template = document.getElementById(templName).innerHTML;
+    mainDiv.innerHTML = Mustache.render(template, params);
+}
+
+function getRenderedTemplate(templNam, params) {
+    var template = document.getElementById(templName).innerHTML;
+    return Mustache.render(template, params);
+}
