7302925b2859c92ba44819c3e53a37f12a3fc788
gsx95 <g.schwarz95@me.com>
2021-01-02 18:52:58 +0100

    put each view in own class

---
+++ statics/js/app.js
@@ -0,0 +1,11 @@
+function start() {
+    initRenderer();
+    showHomeView();
+
+}
+
+function showHomeView() {
+    let homeView = new HomeView();
+    homeView.show();
+}
+
--- statics/js/home.js
+++ statics/js/home.js
@@ -1,37 +1,44 @@
 const ROOMS_ENDPOINT = "http://localhost:3000/rooms?lastRequest=";
 const HOME_VIEW_NAME = "HOME";
 
+class HomeView {
+    lastRequestTime;
 
-let lastRequestTime;
-
-
-function showHome() {
-    CURRENT_VIEW = HOME_VIEW_NAME;
-    initVars();
-    renderWholeView("HomeTemplate");
-    updateRoomTable();
-}
-
-function initVars() {
-    lastRequestTime = 0;
-}
-
-function updateRoomTable() {
-    doGetRequestJSON(ROOMS_ENDPOINT + lastRequestTime,
-        (rooms) => {
-            rooms.sort(function(x, y){
-                return y.created - x.created;
-            });
-            let rows = getRenderedTemplate("HomeTableRowTemplate", {"rooms": rooms});
-            let roomsTableBody = document.getElementById("home-table-body");
-            roomsTableBody.innerHTML = rows;
-        }, (err) => {
-            console.log(err);
-        }, () => {
-            if(CURRENT_VIEW === HOME_VIEW_NAME) {
-                setTimeout(updateRoomTable, 1000);
-            }
+    constructor() {
+        if (HomeView._instance) {
+            return HomeView._instance
         }
-    );
-    lastRequestTime = new Date(new Date().toISOString()).getTime();
+        HomeView._instance = this;
+    }
+
+    show() {
+        CURRENT_VIEW = HOME_VIEW_NAME;
+        this.initVars();
+        renderWholeView("HomeTemplate");
+        this.updateRoomTable();
+    }
+
+    initVars() {
+        this.lastRequestTime = 0;
+    }
+
+    updateRoomTable() {
+        doGetRequestJSON(ROOMS_ENDPOINT + this.lastRequestTime,
+            (rooms) => {
+                rooms.sort(function(x, y){
+                    return y.created - x.created;
+                });
+                let rows = getRenderedTemplate("HomeTableRowTemplate", {"rooms": rooms});
+                let roomsTableBody = document.getElementById("home-table-body");
+                roomsTableBody.innerHTML = rows;
+            }, (err) => {
+                console.log(err);
+            }, () => {
+                if(CURRENT_VIEW === HOME_VIEW_NAME) {
+                    setTimeout(this.updateRoomTable, 1000);
+                }
+            }
+        );
+        this.lastRequestTime = new Date(new Date().toISOString()).getTime();
+    }
 }
