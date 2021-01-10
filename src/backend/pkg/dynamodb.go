faec5fb918516e1c86af74966edba1d30f770b11
gsx95 <g.schwarz95@me.com>
2021-01-10 18:24:38 +0100

    api: get and create rooms, get available rooms

--- src/backend/pkg/dynamodb.go
+++ src/backend/pkg/dynamodb.go
@@ -1,12 +1,13 @@
 package data
 
 import (
+	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
+	"strconv"
 )
-
 
 const tableName = "GeorgGuessrRooms"
 
@@ -15,8 +16,11 @@ var (
 )
 
 type Room struct {
-	ID     string `json:"ID"`
-	Name string `json:"name"`
+	ID         string `json:"id,omitempty"`
+	Name       string `json:"name"`
+	Players    int    `json:"players,omitempty"`
+	MaxPlayers int    `json:"maxPlayers"`
+	Status     string `json:"status,omitempty"`
 }
 
 func GetRoom(roomID string) (*Room, error) {
@@ -43,7 +47,66 @@ func GetRoom(roomID string) (*Room, error) {
 	return item, nil
 }
 
-func CreateRoom(room Room)  error {
+func GetAvailableRooms() ([]*Room, error) {
+
+	exprAttrValues := map[string]*dynamodb.AttributeValue{
+		":creating": {
+			S: aws.String("creating"),
+		},
+		":waiting": {
+			S: aws.String("waiting"),
+		},
+	}
+
+
+	input := &dynamodb.ScanInput{
+		TableName:                 aws.String(tableName),
+		FilterExpression:          aws.String("#status = :creating or #status = :waiting"),
+		ExpressionAttributeValues: exprAttrValues,
+		ExpressionAttributeNames: map[string]*string{
+			"#status": aws.String("status"),
+		},
+	}
+
+	result, err := DynamoClient.Scan(input)
+	if err != nil {
+		return nil, err
+	}
+
+	var items []map[string]string
+	err = dynamodbattribute.UnmarshalListOfMaps(result.Items, &items)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	var rooms []*Room
+	for _, roomItem := range items {
+		room := &Room{
+			ID: roomItem["id"],
+			Status: roomItem["status"],
+			Name: roomItem["name"],
+		}
+		if roomItem["players"] == "" {
+			room.Players = 0
+		} else {
+			players, err := strconv.Atoi(roomItem["players"])
+			if err != nil {
+				return nil, err
+			}
+			room.Players = players
+		}
+		maxPlayers, err := strconv.Atoi(roomItem["maxPlayers"])
+		if err != nil {
+			return nil, err
+		}
+		room.MaxPlayers = maxPlayers
+		rooms = append(rooms, room)
+	}
+	return rooms, nil
+}
+
+func CreateRoom(room Room) error {
 	av, err := dynamodbattribute.MarshalMap(room)
 	if err != nil {
 		return err
