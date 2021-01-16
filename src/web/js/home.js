87f210117f174eaca93ca9d83e475fdecd039154
gsx95 <g.schwarz95@me.com>
2021-01-16 19:06:25 +0100

    add map with polygon drawing and polygon removal for custom area selection to create-room-popup

--- src/web/js/home.js
+++ src/web/js/home.js
@@ -1,4 +1,7 @@
 let lastRequestTime = 0;
+let selectedAreas = [];
+let selectedPolygon;
+let selectionMap;
 
 function showHome() {
     lastRequestTime = 0;
@@ -28,11 +31,99 @@ function updateRoomTable() {
     lastRequestTime = new Date(new Date().toISOString()).getTime();
 }
 
+function initMap() {
+    selectionMap = new google.maps.Map(document.getElementById('selection-map'), {
+        center: {lat: 37.869260, lng: -122.254811},
+        zoom: 1,
+        fullscreenControl: true,
+        mapTypeControl: false,
+        streetViewControl: false,
+    })
+
+    let selectArea = function (polygon) {
+        for(let i = 0; i < selectedAreas.length; i++) {
+          setPolygonColor(selectedAreas[i], '#000000')
+        }
+        setPolygonColor(polygon, '#ff0000')
+        selectedPolygon = polygon
+    }
+
+    let setPolygonColor = function (polygon, color) {
+        var options = {
+          strokeColor: color,
+          strokeOpacity: 0.8,
+          strokeWeight: 3,
+          fillColor: color,
+          fillOpacity: 0.35
+        }
+        options.paths = polygon.getPaths().getArray()
+        polygon.setOptions(options)
+    }
+
+    let deleteSelectedPolygon = function () {
+        selectedPolygon.setMap(null)
+        for(let i = 0;i < selectedAreas.length; i++) {
+          if(selectedAreas[i] == selectedPolygon) {
+            selectedAreas.splice(i, 1);
+            break;
+          }
+        }
+        selectedPolygon = null
+    }
+
+    window.addEventListener('keydown', function(e) {
+      console.log(e.keyCode);
+      if (e.keyCode != 46 && e.keyCode != 8) {
+        return
+      }
+      if(selectedPolygon == null || selectedPolygon == undefined) {
+        return
+      }
+      deleteSelectedPolygon();
+      event.preventDefault();
+    });
+
+    this.drawingManager = new google.maps.drawing.DrawingManager({
+        drawingMode: google.maps.drawing.OverlayType.POLYGON,
+        drawingControl: true,
+        drawingControlOptions: {
+          position: google.maps.ControlPosition.TOP_CENTER,
+          drawingModes: ['polygon']
+        },
+        markerOptions: {icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'},
+    });
+
+    google.maps.event.addListener(this.drawingManager, 'overlaycomplete', function(event) {
+      if (event.type == 'polygon') {
+        event.overlay.setMap(null);
+        if(event.type == 'polygon') {
+          selectedAreas.push(new google.maps.Polygon({
+            paths: event.overlay.getPath().getArray(),
+            strokeColor: '#000000',
+            strokeOpacity: 0.8,
+            strokeWeight: 3,
+            fillColor: '#000000',
+            fillOpacity: 0.35
+          }));
+        }
+        selectedAreas[selectedAreas.length - 1].setMap(selectionMap);
+        selectedAreas[selectedAreas.length - 1].addListener('click', function(e, p1, p2) {
+          selectArea(this)
+        });
+        console.log(selectedAreas.length);
+      }
+    });
+
+    this.drawingManager.setMap(selectionMap);
+}
+
 function showCreateRoom() {
         MicroModal.show('modal-1');
 
         let pwdDropDown = byId("set-pwd");
         let pwdField = byId("pwd");
+        let geoDropDown = byId("set-geo-limits");
+        let geoMap = byId("selection-map");
         let maxPlayerSlider = byId("maxplayer");
         let maxPlayerLabel = byId("maxplayer-label");
         let roundSlider = byId("rounds");
@@ -43,6 +134,10 @@ function showCreateRoom() {
 
         pwdDropDown.onchange = function() {
             pwdField.style.visibility = pwdDropDown.value == "protected" ? "visible" : "hidden";
+            pwdField.style.marginBottom = pwdDropDown.value == "protected" ? "30px" : "0px";
+        }
+        geoDropDown.onchange = function() {
+            geoMap.style.display = geoDropDown.value == "custom" ? "block" : "none";
         }
         maxPlayerSlider.oninput = function() {
             maxPlayerLabel.innerText = "Player: " + maxPlayerSlider.value;
