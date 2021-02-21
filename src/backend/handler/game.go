a6686d79b408f7601bef7f4ccad8bac9d38d7ac4
gsx95 <g.schwarz95@me.com>
2021-02-21 22:54:17 +0100

    working guess, distance calc, timer and result view

--- src/backend/handler/game.go
+++ src/backend/handler/game.go
@@ -46,7 +46,20 @@ func HandleGetGamePosition(request events.APIGatewayProxyRequest) events.APIGate
 		return GenerateResponse("no more rounds", 400)
 	}
 	r := rounds[round - 1]
-	return GenerateResponse(r.PanoID, 200)
+
+	bytes, err := json.Marshal(struct{
+		PanoID string `json:"panoId"`
+		Lat float64 `json:"lat"`
+		Lon float64 `json:"lon"`
+	}{
+		r.PanoID,
+		r.StartPosition.Lat,
+		r.StartPosition.Lon,
+	})
+	if err != nil {
+		return GenerateResponse(fmt.Sprintf("%v", err), 500)
+	}
+	return GenerateResponse(string(bytes), 200)
 }
 
 func HandleGetGameStats(request events.APIGatewayProxyRequest) events.APIGatewayProxyResponse {
@@ -65,12 +78,14 @@ func HandleGetGameStats(request events.APIGatewayProxyRequest) events.APIGateway
 		MaxPlayers int `json:"maxPlayers"`
 		Players int `json:"players"`
 		Status string `json:"status"`
+		TimeLimit int `json:"timeLimit"`
 	}{
 		game.Rounds,
 		game.Name,
 		game.MaxPlayers,
 		game.Players,
 		game.Status,
+		game.TimeLimit,
 	})
 	if err != nil {
 		return GenerateResponse(fmt.Sprintf("%v", err), 500)
