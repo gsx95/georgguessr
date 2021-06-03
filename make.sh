533a8f9cbe1c24331f60369cff6c913ab0c724c3
gsx95 <g.schwarz95@me.com>
2021-06-03 11:51:01 +0200

    make make-script more readable

--- make.sh
+++ make.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-if [[ $1 != "frontend" ]]; then
+mode=$1
+
+build_and_deploy_backend() {
     echo "------------------------"
     echo "Building Backend..."
     echo "------------------------"
@@ -14,7 +16,7 @@ if [[ $1 != "frontend" ]]; then
     echo "------------------------"
     echo "Deploying Backend..."
     echo "------------------------"
-    if [[ $1 = "guided" ]]; then
+    if [[ ${mode} = "guided" ]]; then
         sam deploy --guided
     else
         sam deploy
@@ -26,86 +28,100 @@ if [[ $1 != "frontend" ]]; then
     echo "------------------------"
     echo "Done!"
     echo ""
+}
+
+build_frontend() {
     echo "------------------------"
     echo "Building Frontend..."
     echo "------------------------"
+
+    RES=$(aws cloudformation describe-stack-resources --stack-name georgguessr)
+    API_KEY_ID=$(echo ${RES} | jq -r '.StackResources[]  | select(.ResourceType == "AWS::ApiGateway::ApiKey") | .PhysicalResourceId')
+    API_KEY_VALUE=$(aws apigateway get-api-key --include-value --api-key ${API_KEY_ID} | jq -r '.value')
+    API_ID=$(echo ${RES} | jq -r '.StackResources[]  | select(.ResourceType == "AWS::ApiGateway::RestApi") | .PhysicalResourceId')
+    API_ENDPOINT="https://$API_ID.execute-api.eu-central-1.amazonaws.com/Prod"
+
+    rm -r statics
+    mkdir -p statics
+    mkdir -p statics/guessr
+    mkdir -p statics/guessr/js
+    mkdir -p statics/guessr/css
+    mkdir -p statics/guessr/img
+    mkdir -p statics/guessr/game
+    mkdir -p statics/guessr/createRoom
+    mkdir -p statics/guessr/results
+
+    cp src/web/css/styles.css statics/guessr/css/styles.css
+    cp src/web/css/gamestyles.css statics/guessr/css/gamestyles.css
+    cp src/web/css/results.css statics/guessr/css/results.css
+    cp -r src/web/img/* statics/guessr/img/
+
+    cp src/web/index.html statics/guessr/index.html;
+    cp src/web/game.html statics/guessr/game/index.html;
+    cp src/web/createRoom.html statics/guessr/createRoom/index.html;
+    cp src/web/results.html statics/guessr/results/index.html;
+
+    MAPS_API_KEY=$(cat config.json| jq -r ".maps_api_key");
+    sed -i '' "s/{{maps-api-key}}/$MAPS_API_KEY/g" statics/guessr/index.html;
+    sed -i '' "s,{{api-endpoint}},$API_ENDPOINT,g" statics/guessr/index.html;
+    sed -i '' "s/{{api-key}}/$API_KEY_VALUE/g" statics/guessr/index.html;
+
+    sed -i '' "s/{{maps-api-key}}/$MAPS_API_KEY/g" statics/guessr/game/index.html;
+    sed -i '' "s,{{api-endpoint}},$API_ENDPOINT,g" statics/guessr/game/index.html;
+    sed -i '' "s/{{api-key}}/$API_KEY_VALUE/g" statics/guessr/game/index.html;
+
+    sed -i '' "s/{{maps-api-key}}/$MAPS_API_KEY/g" statics/guessr/createRoom/index.html;
+    sed -i '' "s,{{api-endpoint}},$API_ENDPOINT,g" statics/guessr/createRoom/index.html;
+    sed -i '' "s/{{api-key}}/$API_KEY_VALUE/g" statics/guessr/createRoom/index.html;
+
+    sed -i '' "s/{{maps-api-key}}/$MAPS_API_KEY/g" statics/guessr/results/index.html;
+    sed -i '' "s,{{api-endpoint}},$API_ENDPOINT,g" statics/guessr/results/index.html;
+    sed -i '' "s/{{api-key}}/$API_KEY_VALUE/g" statics/guessr/results/index.html;
+
+    babel src/web/js/* --presets minify > statics/guessr/js/georgguessr.js;
+
+    echo "------------------------"
+    echo "Done!"
+    echo ""
+}
+
+upload_geo_data_to_dynamodb() {
+    echo "------------------------"
+    echo "Uploading to DynamoDB..."
+    echo "------------------------"
+    aws dynamodb batch-write-item --request-items file://src/resources/continents.json
+
+
+    while read l1; do
+        aws dynamodb batch-write-item --request-items "$l1";
+    done <src/resources/cities.jsonData
+
+    while read l2; do
+        aws dynamodb batch-write-item --request-items "$l2";
+    done <src/resources/countries.jsonData
+
+    echo "------------------------"
+    echo "Done!"
+    echo ""
+    echo ""
+}
+###################################
+
+if [[ ${mode} != "frontend" ]]; then
+    build_and_deploy_backend
 fi
-RES=$(aws cloudformation describe-stack-resources --stack-name georgguessr)
-API_KEY_ID=$(echo ${RES} | jq -r '.StackResources[]  | select(.ResourceType == "AWS::ApiGateway::ApiKey") | .PhysicalResourceId')
-API_KEY_VALUE=$(aws apigateway get-api-key --include-value --api-key ${API_KEY_ID} | jq -r '.value')
-API_ID=$(echo ${RES} | jq -r '.StackResources[]  | select(.ResourceType == "AWS::ApiGateway::RestApi") | .PhysicalResourceId')
-API_ENDPOINT="https://$API_ID.execute-api.eu-central-1.amazonaws.com/Prod"
 
-rm -r statics
-mkdir -p statics
-mkdir -p statics/guessr
-mkdir -p statics/guessr/js
-mkdir -p statics/guessr/css
-mkdir -p statics/guessr/img
-mkdir -p statics/guessr/game
-mkdir -p statics/guessr/createRoom
-mkdir -p statics/guessr/results
+build_frontend
 
-cp src/web/css/styles.css statics/guessr/css/styles.css
-cp src/web/css/gamestyles.css statics/guessr/css/gamestyles.css
-cp src/web/css/results.css statics/guessr/css/results.css
-cp -r src/web/img/* statics/guessr/img/
-
-cp src/web/index.html statics/guessr/index.html;
-cp src/web/game.html statics/guessr/game/index.html;
-cp src/web/createRoom.html statics/guessr/createRoom/index.html;
-cp src/web/results.html statics/guessr/results/index.html;
-
-MAPS_API_KEY=$(cat config.json| jq -r ".maps_api_key");
-sed -i '' "s/{{maps-api-key}}/$MAPS_API_KEY/g" statics/guessr/index.html;
-sed -i '' "s,{{api-endpoint}},$API_ENDPOINT,g" statics/guessr/index.html;
-sed -i '' "s/{{api-key}}/$API_KEY_VALUE/g" statics/guessr/index.html;
-
-sed -i '' "s/{{maps-api-key}}/$MAPS_API_KEY/g" statics/guessr/game/index.html;
-sed -i '' "s,{{api-endpoint}},$API_ENDPOINT,g" statics/guessr/game/index.html;
-sed -i '' "s/{{api-key}}/$API_KEY_VALUE/g" statics/guessr/game/index.html;
-
-sed -i '' "s/{{maps-api-key}}/$MAPS_API_KEY/g" statics/guessr/createRoom/index.html;
-sed -i '' "s,{{api-endpoint}},$API_ENDPOINT,g" statics/guessr/createRoom/index.html;
-sed -i '' "s/{{api-key}}/$API_KEY_VALUE/g" statics/guessr/createRoom/index.html;
-
-sed -i '' "s/{{maps-api-key}}/$MAPS_API_KEY/g" statics/guessr/results/index.html;
-sed -i '' "s,{{api-endpoint}},$API_ENDPOINT,g" statics/guessr/results/index.html;
-sed -i '' "s/{{api-key}}/$API_KEY_VALUE/g" statics/guessr/results/index.html;
-
-babel src/web/js/* --presets minify > statics/guessr/js/georgguessr.js;
-
-echo "------------------------"
-echo "Done!"
-echo ""
-
-if [[ $1 = "frontend" ]]; then
+if [[ ${mode} = "frontend" ]]; then
     exit 0
 fi
 
 
-if [[ $1 = "update" ]]; then
+if [[ ${mode} = "update" ]]; then
     exit 0
 fi
 
-
-echo "------------------------"
-echo "Uploading to DynamoDB..."
-echo "------------------------"
-aws dynamodb batch-write-item --request-items file://src/resources/continents.json
-
-
-while read l1; do
-    aws dynamodb batch-write-item --request-items "$l1";
-done <src/resources/cities.jsonData
-
-while read l2; do
-    aws dynamodb batch-write-item --request-items "$l2";
-done <src/resources/countries.jsonData
-
-echo "------------------------"
-echo "Done!"
-echo ""
-echo ""
+upload_geo_data_to_dynamodb
 
 echo "Run the frontend local with ' http-server statics/guessr '"
